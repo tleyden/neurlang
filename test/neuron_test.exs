9be549601c54648bc431059b4be0ec08cf6ef120
Traun Leyden <tleyden@branchcut.com>
2013-04-16 17:36:55 -0700

    more sophisticated function

--- test/neuron_test.exs
+++ test/neuron_test.exs
@@ -1,6 +1,7 @@
 Code.require_file "../test_helper.exs", __FILE__
 
 defmodule NeuronTest do
+
   use ExUnit.Case
 	alias Neurlang.Neuron, as: Neuron
 	alias Neurlang.NeuronProcess, as: NeuronProcess
@@ -8,24 +9,24 @@ defmodule NeuronTest do
 
   test "neuron produces output when process_input function called" do
 		
-		f = fn(_x) -> 1 end
+		f = fn(inputs) -> Enum.reduce inputs, 0, fn(x, acc) -> x + acc end end
     parameters = NeuronParameters.new(activation_function: f)
 		inputs = [1,2]
 		output = Neuron.process_input_vector(parameters, inputs)
 		
-		assert(output == 1)
+		assert(output == 3)
 
 	end
 
 	test "neuron process produces output when enough inputs provided" do
 
-		f = fn(_x) -> 1 end
+		f = fn(inputs) -> Enum.reduce inputs, 0, fn(x, acc) -> x + acc end end
     parameters = NeuronParameters.new(activation_function: f)
 		neuron_process_state = NeuronProcessState.new(parameters: parameters, 
 																									input_nodes: [self()],
 																								  output_nodes: [self()])
 		{ :ok, pid } = NeuronProcess.start_link(neuron_process_state)
-		NeuronProcess.process_input(pid, {self(), 0.73})
+		NeuronProcess.process_input(pid, {self(), 1})
 
 		received = receive do
 			{ :output, value} -> 
