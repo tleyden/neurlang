2e4d306f30e8568753e06828c04fe0e8d4b12f48
Traun Leyden <tleyden@branchcut.com>
2013-04-19 09:56:38 -0700

    move test to neuralnettest

--- test/neuralnet_test.exs
+++ test/neuralnet_test.exs
@@ -1,61 +1,56 @@
 Code.require_file "../test_helper.exs", __FILE__
 
+defrecord MockNode, pid: nil do
+	@defmodule """
+  Useful to allow neurons and actuators send messages to the test process
+  """
+end
+
 defmodule NeuralNetworkTest do
 
   use ExUnit.Case
 	alias Neurlang.Neuron, as: Neuron
+	alias Neurlang.NeuronProcess, as: NeuronProcess
+	alias Neurlang.Sensor, as: Sensor
+	alias Neurlang.SensorProcess, as: SensorProcess
+	alias Neurlang.Actuator, as: Actuator
+	alias Neurlang.ActuatorProcess, as: ActuatorProcess
 	alias Neurlang.TestHelper, as: TestHelper
 
-	test "create a full neural net and feed data through it" do
+	test "create a full neural net with one neuron and feed data through it" do
 
-		# NOTE: doesn't work yet .. just building the API
+		neuron = Neuron.new( id: make_ref(), bias: bias(10), activation_function: function(:identity, 1) )
+		neuron = NeuronProcess.start_link( neuron)
 
-		f = function(:summation, 3)
+		sensor = Sensor.new( id: make_ref(), output_vector_length: 5 )
+		sensor = SensorProcess.start_link( sensor ) 
 
-		# create network objects
-		sensor1 = Sensor.new(id: make_ref())
-		sensor2 = Sensor.new(id: make_ref())
-		neuron1_1 = Neuron.new(id: make_ref(), bias: bias(10), activation_function: f)
-		neuron1_2 = Neuron.new(id: make_ref(), bias: bias(-10), activation_function: f)
-		neuron2_1 = Neuron.new(id: make_ref(), bias: bias(-10), activation_function: f)
-		actuator = Actuator.new(id: make_ref())
+		actuator = Actuator.new( id: make_ref() )
+		actuator = ActuatorProcess.start_link( actuator )
+	
+		sensor = SensorProcess.add_outbound_connection( sensor, neuron ) 
+		neuron = NeuronProcess.add_inbound_connection( neuron, sensor, weight([20, 20, 20, 20, 20]) ) 
+		
+		neuron = NeuronProcess.add_outbound_connection( neuron, actuator )
+		actuator = ActuatorProcess.add_inbound_connection( actuator, neuron )
+		actuator = ActuatorProcess.add_outbound_connection( actuator, MockNode.new( pid: self() ) )
 
-		# start network processes
-		{ :ok, sensor1 } = SensorProcess.start_link(sensor1)
-		{ :ok, sensor2 } = SensorProcess.start_link(sensor2)
-		{ :ok, neuron1_1 } = NeuronProcess.start_link(neuron1_1)
-		{ :ok, neuron1_2 } = NeuronProcess.start_link(neuron1_2)
-		{ :ok, neuron2_1 } = NeuronProcess.start_link(neuron2_1)
+		SensorProcess.sync_with_outputs(sensor, [1, 1, 1, 1, 1])
 
-		# this test process will act as the actuator process
-		actuator = actuator.pid(self())
-
-		# connect network
-		{sensor1, _neuron1_1} = Connector.connect(sensor1, neuron1_1, weight(20))
-		{sensor1, _neuron1_2} = Connector.connect(sensor1, neuron1_2, weight(20))
-		{sensor2, _neuron1_1} = Connector.connect(sensor2, neuron1_1, weight(20))
-		{sensor2, _neuron1_2} = Connector.connect(sensor2, neuron1_2, weight(20))
-		{_neuron1_1, _neuron2_1} = Connector.connect(neuron1_1, neuron2_1, weight(20))
-		{_neuron1_2, _neuron2_1} = Connector.connect(neuron1_2, neuron2_1, weight(20))
-		{_neuron2_1, _actuator} = Connector.connect(neuron2_1, actuator)
-
-		# put values into network, see if expected result comes back
-		SensorProcess.send_input(sensor1, 1)
-		assert(receive_actuator_output() == :timeout)  # neuron still waiting for other input
-
-		SensorProcess.send_input(sensor2, 2)
-		assert(receive_actuator_output() != :timeout)  # should get a value back.  TODO: which value?
+		value = receive do
+			{pid, :forward, output} -> 
+				assert output == [ 110 ]  
+			any ->
+				assert false, "Got unexpected message: #{inspect(any)}"
+		after
+			1000 -> assert false, "Did not receive any output from actuator in time"
+		end
 
 	end
 
-	def summation(inputs, weights, bias) do
-		TestHelper.summation(inputs, weights, bias)
+	def identity(x) do
+		x
 	end
-
-	def receive_actuator_output() do
-		:timeout  # TODO
-	end
-
 
 	def weight(x) do
 		x
@@ -64,6 +59,5 @@ defmodule NeuralNetworkTest do
 	def bias(x) do
 		x
 	end
-
 
 end
--- test/neuronprocess_test.exs
+++ test/neuronprocess_test.exs
@@ -1,63 +1,12 @@
 Code.require_file "../test_helper.exs", __FILE__
 
-defrecord MockNode, pid: nil do
-	@defmodule """
-  Useful to allow neurons and actuators send messages to the test process
-  """
-end
-
 defmodule NeuronProcessTest do
 
   use ExUnit.Case
 	alias Neurlang.Neuron, as: Neuron
-	alias Neurlang.NeuronProcess, as: NeuronProcess
-	alias Neurlang.Sensor, as: Sensor
-	alias Neurlang.SensorProcess, as: SensorProcess
-	alias Neurlang.Actuator, as: Actuator
-	alias Neurlang.ActuatorProcess, as: ActuatorProcess
-	alias Neurlang.TestHelper, as: TestHelper
 
-	test "new v2 connected neuron" do
-
-		neuron = Neuron.new( id: make_ref(), bias: bias(10), activation_function: function(:identity, 1) )
-		neuron = NeuronProcess.start_link( neuron)
-
-		sensor = Sensor.new( id: make_ref(), output_vector_length: 5 )
-		sensor = SensorProcess.start_link( sensor ) 
-
-		actuator = Actuator.new( id: make_ref() )
-		actuator = ActuatorProcess.start_link( actuator )
-	
-		sensor = SensorProcess.add_outbound_connection( sensor, neuron ) 
-		neuron = NeuronProcess.add_inbound_connection( neuron, sensor, weight([20, 20, 20, 20, 20]) ) 
-		
-		neuron = NeuronProcess.add_outbound_connection( neuron, actuator )
-		actuator = ActuatorProcess.add_inbound_connection( actuator, neuron )
-		actuator = ActuatorProcess.add_outbound_connection( actuator, MockNode.new( pid: self() ) )
-
-		SensorProcess.sync_with_outputs(sensor, [1, 1, 1, 1, 1])
-
-		value = receive do
-			{pid, :forward, output} -> 
-				assert output == [ 110 ]  
-			any ->
-				assert false, "Got unexpected message: #{inspect(any)}"
-		after
-			1000 -> assert false, "Did not receive any output from actuator in time"
-		end
-
-	end
-
-	def identity(x) do
-		x
-	end
-
-	def weight(x) do
-		x
-	end
-
-	def bias(x) do
-		x
+	test "test neuron in isolation" do
+		assert true
 	end
 
 end
