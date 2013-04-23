7fbe2674dbb895dfa19f488083eaf605513c5a96
Traun Leyden <tleyden@branchcut.com>
2013-04-23 09:57:39 -0700

    rename state -> node to be clearer

--- lib/neurlang/nodeprocess.ex
+++ lib/neurlang/nodeprocess.ex
@@ -2,7 +2,13 @@
 defmodule Neurlang.NodeProcess do
 
 	@moduledoc """
-  This wraps a neuron and provides functionality to communicate with other nodes (sensors, neurons, actuators).  
+  This provides the _actor_ functionality of neural elements in neurlang.  
+
+  Each type of element (sensor, neuron, actuator) can be _wrapped_ by this nodeprocess, 
+  which allows it to communicate with other nodes (sensors, neurons, actuators) using 
+  async message passing.
+
+	The gen_server state is represented by the appropriate node record (sensor, neuron, actuator).
   """
 	use GenServer.Behaviour
 	alias Neurlang.Accumulator, as: Accumulator
@@ -13,11 +19,11 @@ defmodule Neurlang.NodeProcess do
 	@doc """
   Start the process
 
-  * `state` - a Neuron, Actuator, or Sensor record
+  * `node` - a Neuron, Actuator, or Sensor
   """
-	def start_link(state) do
-		{:ok, pid} = :gen_server.start_link(__MODULE__, state, [])	
-		state.pid(pid)
+	def start_link( node ) do
+		{:ok, pid} = :gen_server.start_link(__MODULE__, node, [])	
+		node.pid(pid)
 	end
 
 	@doc """
@@ -49,69 +55,68 @@ defmodule Neurlang.NodeProcess do
 		:gen_server.cast( ConnectedNode.pid( node ), :sync )
 	end
 
-
 	## Private
 
-	defp handle_input( {from_pid, input_value}, state ) do
+	defp handle_input( {from_pid, input_value}, node ) do
 		"""
     Handle a new incoming input value from another node in the neural net and
     send output to all connected output nodes
     """
-		state = Accumulator.update_barrier_state(state, {from_pid, input_value})
+		node = Accumulator.update_barrier_state( node, {from_pid, input_value} )
 
-		if Accumulator.is_barrier_satisfied?(state) do
-			output = Accumulator.compute_output( state )
-			Accumulator.propagate_output( state, output )
-			state = Accumulator.create_barrier( state )
+		if Accumulator.is_barrier_satisfied?( node ) do
+			output = Accumulator.compute_output( node )
+			Accumulator.propagate_output( node, output )
+			node = Accumulator.create_barrier( node )
 		end
 
-		state
+		node
 	end
 	
 	## OTP 
 
 	@doc false
-	def init( state ) do
-		state = Accumulator.create_barrier( state )
-		state = state.pid( self() ) 
-		{ :ok, state }
+	def init( node ) do
+		node = Accumulator.create_barrier( node )
+		node = node.pid( self() ) 
+		{ :ok, node }
 	end
 
 	@doc false
-	def handle_info({from_pid, :forward, input_value}, state) do
-		state = handle_input({from_pid, input_value}, state)
-		{ :noreply, state }
+	def handle_info( {from_pid, :forward, input_value}, node ) do
+		node = handle_input( {from_pid, input_value}, node )
+		{ :noreply, node }
 	end
 
 	@doc false
-	def handle_cast({from_pid, input_value}, state) do
-		state = handle_input({from_pid, input_value}, state)
-		{ :noreply, state }
+	def handle_cast( {from_pid, input_value}, node ) do
+		node = handle_input( {from_pid, input_value}, node )
+		{ :noreply, node }
 	end
 
 	@doc false
-	def handle_cast(:sync, state) do
-		output = Accumulator.sync( state )
-		Accumulator.propagate_output( state, output )
-		{ :noreply, state }
+	def handle_cast( :sync, node ) do
+		output = Accumulator.sync( node )
+		Accumulator.propagate_output( node, output )
+		{ :noreply, node }
 	end
 
 	@doc false
-	def handle_call( {:add_inbound_connection, { node, weights }}, _from, state) do
-		state = ConnectedNode.add_inbound_connection( state, node, weights )
-		{ :reply, state, state }
+	def handle_call( {:add_inbound_connection, { from_node, weights }}, _from_pid, node) do
+		node = ConnectedNode.add_inbound_connection( node, from_node, weights )
+		{ :reply, node, node }
 	end
 
 	@doc false
-	def handle_call( {:add_inbound_connection, node}, _from, state) do
-		state = ConnectedNode.add_inbound_connection( state, node )
-		{ :reply, state, state }
+	def handle_call( {:add_inbound_connection, from_node}, _from_pid, node ) do
+		node = ConnectedNode.add_inbound_connection( node, from_node )
+		{ :reply, node, node }
 	end
 
 	@doc false
-	def handle_call( {:add_outbound_connection, node}, _from, state) do
-		state = ConnectedNode.add_outbound_connection( state, node )
-		{ :reply, state, state }
+	def handle_call( {:add_outbound_connection, to_node}, _from_pid, node ) do
+		node = ConnectedNode.add_outbound_connection( node, to_node )
+		{ :reply, node, node }
 	end
 
 
