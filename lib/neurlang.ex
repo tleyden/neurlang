aaf1b74d80e6343d495e8d92f2e315147dd62071
Traun Leyden <tleyden@branchcut.com>
2013-04-25 15:12:47 -0700

    put aliases all in one place.   add a few types.

--- lib/neurlang.ex
+++ lib/neurlang.ex
@@ -1,2 +1,16 @@
 defmodule Neurlang do
+  defmacro __using__(_) do
+    quote do
+      alias Neurlang.Neuron, as: Neuron
+      alias Neurlang.NodeProcess, as: NeuronProcess
+      alias Neurlang.Sensor, as: Sensor
+      alias Neurlang.NodeProcess, as: SensorProcess
+      alias Neurlang.Actuator, as: Actuator
+      alias Neurlang.NodeProcess, as: ActuatorProcess
+    end
+  end
+
+  @type neuro_node :: Actuator.t | Neuron.t | Sensor.t
+  @type barrier_entry :: {pid, list(number)}
+
 end
